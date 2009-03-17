4865ba02e8bb3fcf33e5484e929a28ee32fc85f1
andrei.sambra <andrei.sambra@ff8e87fc-122d-11de-a236-ef2eabcd8114>
2009-03-17 14:26:29 +0000

    Minor changes; iproved stability.

--- trunk/net/main.c
+++ trunk/net/main.c
@@ -1,6 +1,6 @@
 /*
  *	Main function of promisc tool.
- *	v0.3
+ *	v0.3.1
  *
  *	Author: Andrei SAMBRA (andrei.sambra@gmail.com)
  *	Licence type: GNU GPLv3
@@ -9,40 +9,66 @@
 #include <libnet.h>
 #include <pthread.h>
 
+#include "func.h"
 #include "arp.h"
 #include "icmp.h"
-#include "mdns.h"
+
+void usage(char *prog);
 
 int main(int argc, char *argv[])
 {
-    if ((argc < 3) || (getuid() != 0))
-    {
-	printf("\nUSAGE (with root priviledges): %s <proto> <device>\n", argv[0]);
-	printf("example: %s arp eth0\n\n", argv[0]);
-        exit(1);
-    }
+	char *device, *proto;
+	int opt=0;
 
-    /* decide which protocol to use */
-    if (strcmp(argv[1], "arp") == 0)
-    {
-        printf("Packet shaping using libnet 1.1: ARP [LINK]\n");
-	send_arp(argv[2]);		/* Use ARP protocol */
-    }
-    else if (strcmp(argv[1], "icmp") == 0)
-    {
-	printf("Packet shaping using libnet 1.1: ICMP [LINK]\n");
-	send_icmp(argv[2]);		/* Use ICMP protocol */
-    }
-    else if (strcmp(argv[1], "mdns") == 0)
-    {
-	printf("Packet shaping using libnet 1.1: UDP mDNS [LINK]\n");
-	send_mdns(argv[2]);             /* Use UDP protocol */
-    }
-    else
-    {
-        printf("Unknown protocol, please specify either arp, icmp or mdns.\n\n");
-        exit(EXIT_FAILURE);
-    }
+	while ((opt = getopt(argc, argv, "hp:i:")) != -1) {
+		switch (opt) {
+			case 'p':
+				proto = optarg;
+				break;
+			case 'h':
+				usage(argv[0]);
+				exit(EXIT_SUCCESS);
+			case 'i':
+				device = optarg;
+				break;
+			default:
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+	
+	printf("argc=%d | optind=%d\n", argc, optind);
+
+	if (optind != 5) {
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* decide which protocol to use */
+	if (strcmp(proto, "arp") == 0)
+	{
+		printf("Packet shaping using libnet 1.1: ARP [LINK IPv4]\n");
+		send_arp(device);             /* Use ARP protocol */
+		
+	}
+	else if (strcmp(proto, "icmp") == 0)
+	{
+		printf("Packet shaping using libnet 1.1: ICMPv4 [LINK IPv4]\n");
+		send_icmp(device);             /* Use ICMPv4 protocol */
+	}
+	else
+	{
+		printf("Unknown protocol, please specify either arp, icmp.\n\n");
+		exit(EXIT_FAILURE);
+	}
   
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
+
+void usage(char *prog)
+{
+
+	printf("\nUSAGE (as root): %s -p <proto> -i <device> \n\n", prog);
+	printf("example: %s -p icmp -i eth0 \n", prog);
+}
+
